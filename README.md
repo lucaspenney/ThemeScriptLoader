f346dcd0cf90a43825bb4b73e66c6d8617547f48
Lucas Penney <lucaspenney@gmail.com>
2014-01-07 20:17:42 -0800

    Added 'themescript' shortcode
    Specifying a 'name' attribute will load that script from the active
    theme's 'scripts' directory. For example, [themescript name=test.js]
    will create a script tag for scripts/test.js.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Theme Script Loader
 ==============
 
-Theme Script Loader lets you specify script files inside of WordPress pages/posts to be inserted from your active theme's scripts directory. 
+Theme Script Loader lets you specify script files inside of WordPress pages/posts to be inserted from your active theme's 'scripts' directory.
+
+To load a script, insert the themescript shortcode specifying the name of the file, including the .js file extension.
+
+	[themescript name=myscript.js]
+
+This will load the file named myscript.js in your currently active theme's 'scripts' directory (this directory does not exist by default, you may have to create it). 
--- themescriptloader.php
+++ themescriptloader.php
@@ -1,17 +1,25 @@
 <?php
 /**
  * Plugin Name: Theme Script Loader
- * Plugin URI: 
- * Description: Allows you to add scripts from your active theme's directory
+ * Plugin URI: https://github.com/lucaspenney/ThemeScriptLoader
+ * Description: Allows you to add scripts from your current theme's 'scripts' directory
  * Version: 0.1.0
  * Author: Lucas Penney
  * Author URI: http://lucaspenney.com/
  * License: 
  */
 
-function contentFilter($content) {
-	return $content;
+function loadThemeScript($atts) {
+	if (strpos($atts['name'], '.js') !== false) {
+		$script = "<script src='";
+		$script .= get_template_directory_uri() . "/scripts/";
+		$script .= $atts['name'];
+		$script .= "'></script>";
+		return $script;
+	}
+	return "";
 }
-add_filter( 'the_content', 'contentFilter'); 
+
+add_shortcode( 'themescript', 'loadThemeScript' );
 
 ?>
